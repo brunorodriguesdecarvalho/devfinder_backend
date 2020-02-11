b867a613b6d8df36774e8ec3958eae4fba241558
brunorodriguesdecarvalho <rodrigues_b@hotmail.com>
2020-02-11 13:25:53 -0300

    Inclusão da variavel ambiente da porta Heroku.

--- src/Index.js
+++ src/Index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes = require('./routes');
-
 const app = express();
+const PORT = process.env.PORT || 3333
 
 mongoose.connect('mongodb+srv://devfinder:15315300@cluster0-z3yzk.azure.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
